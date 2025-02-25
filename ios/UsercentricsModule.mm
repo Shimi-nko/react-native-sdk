a119b6e36a65d584fd71818a15650bb3969e2f46
tomaszachar <tomike2212@gmail.com>
2025-02-25 09:30:31 +0100

    fea(Usercentrics-ios): use c++ file for integration

---
+++ ios/UsercentricsModule.mm
@@ -0,0 +1,176 @@
+#import "Usercentrics.h"
+
+#if __has_include("react_native_usercentrics-Swift.h")
+#import "react_native_usercentrics-Swift.h"
+#else
+#import "react_native_usercentrics/react_native_usercentrics-Swift.h"
+#endif
+
+@implementation Usercentrics {
+    UsercentricsImpl *usercentricsImpl;
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        usercentricsImpl = [UsercentricsImpl new];
+    }
+    return self;
+}
+
+RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(configure:(NSDictionary *)options
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl configure:options];
+}
+
+RCT_EXPORT_METHOD(showFirstLayer:(NSDictionary *)options
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl showFirstLayer:options resolve:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(showSecondLayer:(NSDictionary *)options
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl showSecondLayer:options resolve:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(restoreUserSession:(NSString *)controllerId
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl restoreUserSession:controllerId resolve:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(isReady:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl isReady:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(getTCFString:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl getTCFString:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(getControllerId:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl getControllerId:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(getABTestingVariant:(RCTPromiseResolveBlock)resolve
+                reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl getABTestingVariant:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(getConsents:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl getConsents:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(getCMPData:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl getCMPData:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(getUserSessionData:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl getUserSessionData:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(getUSPData:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl getUSPData:resolve reject:reject];
+}
+RCT_EXPORT_METHOD(getTCFData:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl getTCFData:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(getAdditionalConsentModeData:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl getAdditionalConsentModeData:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(changeLanguage:(NSString *)language
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl changeLanguage:language resolve:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(acceptAllForTCF:(NSInteger *)fromLayer
+                  consentType:(NSInteger)consentType
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl acceptAllForTCF:fromLayer consentType:consentType resolve:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(acceptAll:(NSInteger *)consentType
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl acceptAll:consentType resolve:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(denyAllForTCF:(NSInteger *)fromLayer
+                  consentType:(NSInteger)consentType
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl denyAllForTCF:fromLayer consentType:consentType resolve:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(denyAll:(NSInteger *)consentType
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl denyAll:consentType resolve:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(saveDecisionsForTCF:(NSDictionary *)tcfDecisions
+                  fromLayer:(NSInteger)fromLayer
+                  serviceDecisions:(NSArray *)serviceDecisions
+                  consentType:(NSInteger)consentType
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl saveDecisionsForTCF:tcfDecisions fromLayer:fromLayer serviceDecisions: serviceDecisions consentType:consentType resolve:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(saveDecisions:(NSArray *)serviceDecisions
+                  consentType:(NSInteger)consentType
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl saveDecisions:serviceDecisions consentType:consentType resolve:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(saveOptOutForCCPA:(BOOL *)isOptedOut
+                  consentType:(NSInteger)consentType
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl saveOptOutForCCPA:isOptedOut consentType:consentType resolve:resolve reject:reject];
+}
+
+RCT_EXPORT_METHOD(setCMPId:(NSInteger *)id) {
+    [usercentricsImpl setCMPId:id];
+}
+
+RCT_EXPORT_METHOD(setABTestingVariant:(NSString *)variant) {
+    [usercentricsImpl setABTestingVariant:variant];
+}
+
+RCT_EXPORT_METHOD(track:(NSInteger *)event) {
+    [usercentricsImpl track:event];
+}
+
+RCT_EXPORT_METHOD(clearUserSession:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject) {
+    [usercentricsImpl clearUserSession:resolve reject:reject];
+}
+
+#ifdef RCT_NEW_ARCH_ENABLED
+- (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:
+    (const facebook::react::ObjCTurboModule::InitParams &)params
+{
+    return std::make_shared<facebook::react::NativeUsercentricsSpecJSI>(params);x
+}
+#endif
+
+@end
